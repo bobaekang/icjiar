c4a6618716918596e24f09b8183604b862673819
Bobae Kang <Bobae.Kang@illinois.gov>
2018-08-13 12:20:33 -0500

    fix region mismatch; use "standard" county names

--- data-raw/regions.R
+++ data-raw/regions.R
@@ -1,5 +1,5 @@
 # create data
-northern = c("Jo Daviess", "Stephenson", "Winnebago", "Boone", "McHenry", "Lake", "Carroll", "Ogle", "Whiteside", "Lee", "De Kalb", "Kane", "Du Page", "La Salle", "Kendall", "Grundy", "Will")
+northern = c("Jo Daviess", "Stephenson", "Winnebago", "Boone", "McHenry", "Lake", "Carroll", "Ogle", "Whiteside", "Lee", "DeKalb", "Kane", "DuPage", "LaSalle", "Kendall", "Grundy", "Will")
 central = c("Rock Island", "Henry", "Bureau", "Putnam", "Mercer", "Henderson", "Warren", "Knox", "Stark", "Marshall", "Peoria", "Woodford", "Livingston", "Kankakee", "Iroquois", "Hancock", "McDonough", "Fulton", "Tazewell", "McLean", "Ford", "Adams", "Schuyler", "Mason", "Logan", "De Witt", "Brown", "Cass", "Menard", "Pike", "Scott", "Morgan", "Sangamon", "Christian", "Macon", "Piatt", "Champaign", "Vermilion", "Greene", "Macoupin", "Montgomery", "Shelby", "Moultrie", "Douglas", "Coles", "Edgar")
 southern = c("Calhoun", "Jersey", "Cumberland", "Clark", "Madison", "Bond", "Fayette", "Effingham", "Jasper", "Crawford", "St. Clair", "Clinton", "Marion", "Clay", "Richland", "Lawrence", "Monroe", "Washington", "Jefferson", "Wayne", "Edwards", "Wabash", "Randolph", "Perry", "Franklin", "Hamilton", "White", "Jackson", "Williamson", "Saline", "Gallatin", "Union", "Johnson", "Pope", "Hardin", "Alexander", "Pulaski", "Massac")
 
