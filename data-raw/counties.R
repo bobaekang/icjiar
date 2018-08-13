d6ca35825f5a1f2e6a5abd8f2c4c01392cd32b6d
Bobae Kang <Bobae.Kang@illinois.gov>
2018-08-13 12:20:33 -0500

    fix region mismatch; use "standard" county names

--- data-raw/counties.R
+++ data-raw/counties.R
@@ -1,12 +1,11 @@
-library(dplyr)
+# get pipe
+`%>%` <- dplyr::`%>%`
 
-
-# get standard county names
-source("data-raw/std_county_names.R")
 
 # get rural-urban categories
 rural_urban <- read.csv('data-raw/rural_urban.csv', stringsAsFactors=FALSE)
 colnames(rural_urban) <- c("fips", "county", "region", "rural_urban_2010", "rural_percentile_2010")
+
 
 # import shapefile
 counties <- sp::spTransform(
@@ -14,26 +13,13 @@ counties <- sp::spTransform(
   CRSobj = sp::CRS("+proj=longlat +ellps=GRS80")
 )
 
-# standard name to join
-to_join <-
-  counties@data %>%
-  distinct(CNTYNAM_LO, FIPS_CODE) %>%
-  arrange(CNTYNAM_LO) %>%
-  cbind(name = county_std) %>%
-  mutate(
-    county = as.character(CNTYNAM_LO),
-    name = ifelse(county == "LaSalle", "La Salle", as.character(name)),
-    name = ifelse(county == "Lake", "Lake", as.character(name))
-  ) %>%
-  distinct(FIPS_CODE, name)
-
 
 # prepare data
 counties@data <-
   counties@data %>%
-  left_join(to_join) %>%
-  transmute(
-    name = name,
+  dplyr::transmute(
+    name = as.character(CNTYNAM_LO),
+    name = ifelse(name == "DeWitt", "De Witt", name),
     type = COUNTYTYPE,
     region = ifelse(
       as.character(CNTYNAM_LO) == "Cook", "Cook", ifelse(
@@ -42,11 +28,19 @@ counties@data <-
         )
       )
     ),
-    circuit = as.integer(as.character(CIRCUIT)),
+    # fix region information for Kankakee and LaSalle (08132018)
+    region = ifelse(
+      FIPS_CODE == 17091, "Central", ifelse(
+        FIPS_CODE == 17099, "Northern", region
+      )
+    ),
+    circuit = as.character(CIRCUIT),
     idoc = as.integer(as.character(IDOC_CODE)),
     fips = as.integer(as.character(FIPS_CODE))
   ) %>%
-  left_join(select(rural_urban, fips, rural_urban_2010, rural_percentile_2010))
+  dplyr::left_join(
+    dplyr::select(rural_urban, fips, rural_urban_2010, rural_percentile_2010)
+  )
 
 
 # use data
